4a0784f1a0c07ada2a307c9ea410be522da2065e
Jinqchen <44107859+Jinqchen@users.noreply.github.com>
2021-12-09 15:51:51 -0500

    ezauction rename

--- ezauction2_backup.sql
+++ ezauction2_backup.sql
@@ -1,6 +1,8 @@
+CREATE DATABASE  IF NOT EXISTS `ezauction` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ezauction`;
 -- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: ezauction2
+-- Host: 127.0.0.1    Database: ezauction
 -- ------------------------------------------------------
 -- Server version	8.0.22
 
@@ -331,4 +333,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-09 15:40:56
+-- Dump completed on 2021-12-09 15:49:19
